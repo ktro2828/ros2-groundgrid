ee32af76334bd1aefc0b654612d1d5e8b8c3d22d
ktro2828 <kotaro.uetake@tier4.jp>
2025-02-13 14:12:58 +0900

    refactor: remove unused point type
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- groundgrid/include/groundgrid/point_types.hpp
+++ groundgrid/include/groundgrid/point_types.hpp
@@ -40,45 +40,10 @@ struct EIGEN_ALIGN16 PointXYZIL
 
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 };
-
-/**
- * @brief Extended 3D point with intensity, ring number, distance, and azimuth.
- */
-struct EIGEN_ALIGN16 PointXYZIDRA
-{
-  // Constructors
-  PointXYZIDRA() : x(0.0f), y(0.0f), z(0.0f), intensity(0.0f), distance(0.0f), ring(0), azimuth(0)
-  {
-  }
-
-  PointXYZIDRA(
-    float _x, float _y, float _z, float _intensity, float _distance, std::uint16_t _ring,
-    std::uint16_t _azimuth)
-  : x(_x), y(_y), z(_z), intensity(_intensity), distance(_distance), ring(_ring), azimuth(_azimuth)
-  {
-  }
-
-  // Member variables
-  float x;
-  float y;
-  float z;
-  float intensity;
-  float distance;
-  std::uint16_t ring;
-  std::uint16_t azimuth;
-
-  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-};
-
 };  // namespace groundgrid
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
   groundgrid::PointXYZIL,
   (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(std::uint16_t, label, label))
-
-POINT_CLOUD_REGISTER_POINT_STRUCT(
-  groundgrid::PointXYZIDRA,
-  (float, x, x)(float, y, y)(float, z, z)(float, intensity, intensity)(float, distance, distance)(
-    std::uint16_t, ring, ring)(std::uint16_t, azimuth, azimuth))
 
 #endif  // GROUNDGRID__POINT_TYPES_HPP_
